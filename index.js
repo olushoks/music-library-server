d49c462a9af746c2e89a72308e353f4886f0498a
Olukayode <olukayodeshokunbi@gmail.com>
2021-02-23 18:47:29 -0500

    Initialized Serverto listen on PORT 3000

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const express = require("express");
 
 // Initialize express
 const app = express();
+
+// Initialize PORT
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World, I am express</h1>");
+});
