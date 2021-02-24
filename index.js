343a9e7ed18dfc8abf3c92b9fc62d79640b6b64c
Olukayode <olukayodeshokunbi@gmail.com>
2021-02-23 19:56:27 -0500

    Created POST method that enables adding songs to the server

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const repoContext = require("./repository/repository-wrapper");
 
 // Initialize express
 const app = express();
+
+// Initialize Miiddleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Initialize PORT
 const PORT = process.env.PORT || 3000;
@@ -10,4 +15,10 @@ app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World, I am express</h1>");
+});
+
+app.post("/api/songs", (req, res) => {
+  const songToAdd = req.body;
+  const newSong = repoContext.songs.createSong(songToAdd);
+  res.send(newSong);
 });
