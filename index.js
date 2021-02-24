02b833760f5afa312057d15005e72b5fe78c8c21
Olukayode <olukayodeshokunbi@gmail.com>
2021-02-24 07:42:37 -0500

    Updated GET URL Endpoint to indicate what action is been performed: From "/api/songs" to "api/songs/get"

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
 
 // REQUEST ALL SONGS IN DATABASE
-app.get("/api/songs", (req, res) => {
+app.get("/api/songs/get", (req, res) => {
   const songs = repoContext.songs.findAllSongs();
   res.send(songs);
 });
