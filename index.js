8bfb2896228b21da7e963f7be06f8057d8180e81
Olukayode <olukayodeshokunbi@gmail.com>
2021-03-05 06:51:41 -0500

    Changed PORT from 3000 to 5000

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Initialize PORT
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
 
